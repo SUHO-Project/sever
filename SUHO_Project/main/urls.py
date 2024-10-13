1d0f88155a6d4c04fa88d84a0b36bb01a7055100
Angayeon <agy125@naver.com>
2024-10-13 17:56:41 +0900

    upload

--- SUHO_Project/main/urls.py
+++ SUHO_Project/main/urls.py
@@ -4,5 +4,6 @@ from . import views
 
 
 urlpatterns = [
-    path('', views.index, name='index'),
+    path('', views.kiosk, name='kiosk'),
+    path('cafemain', views.cafemain, name='cafemain'),
 ]
--- SUHO_Project/main/views.py
+++ SUHO_Project/main/views.py
@@ -1,5 +1,8 @@
 from django.shortcuts import render
 
 # Create your views here.
-def index(request):
-    return render(request,'index.html')
+def kiosk(request):
+    return render(request,'kiosk.html')
+
+def cafemain(request):
+    return render(request,'cafemain.html')
